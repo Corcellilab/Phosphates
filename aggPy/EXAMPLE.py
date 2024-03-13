f767ac9692d56fdff06a3b9e296fce34b630acd8
nvasconez <119025867+nvasconez@users.noreply.github.com>
2024-03-13 12:29:53 -0400

    Update EXAMPLE.py

--- aggPy/EXAMPLE.py
+++ aggPy/EXAMPLE.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 from aggPy import *
 
-hbonds = Analysis('example.json', 'hbond')
+hbonds = Analysis('MDin.json', 'hbond')
 hbonds.hCoordination()
 
-dists = hbonds.aggregate('Distance')
-plt.hist(dists, bins=100)
+i = Analysis.aggregate(hbonds, 'Distance')
+plt.hist(i, bins=100)
 plt.show()
 
-hbonds.std_dev('Coordinations',bin_width=25)
-print(hbonds.CoordinationsStdev)
+j = Analysis.std_dev(hbonds, 'Coordinations')
+print(j)
 
-hbonds.average('Coordinations')
-print(hbonds.CoordinationsAvg)
+x = Analysis.average(hbonds, 'Coordinations')
+print(x)
 
-mol_ct, sys_ct = hbonds.timeCorr()
+mol_ct, sys_ct = hbonds.timeCorr(hbonds)
 plt.plot(list(sys_ct.keys()), list(sys_ct.values()))
 plt.show()
+
